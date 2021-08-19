b02d463370b56fde6ecf17e29e61d71b62c01c6f
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-08-19 09:10:12 -0300

    added a few lines to test code

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-# Logging import and settings
 import logging
+# Erasing previous log
+with open('log.log', 'w'):
+    pass
+
+# Setting the logger
 logging.basicConfig(format="%(asctime)s %(levelname)-8s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s", level=logging.DEBUG,
-                    filename='log.txt')
+                    filename='log.log')
 log = logging.getLogger('cp_flue_gases')
 
 # Other imports
+
+
+# Test Area
+from data.db_manager import Database
+
+host_name = 'stored_info'
+
+app_db = Database(host_name)
