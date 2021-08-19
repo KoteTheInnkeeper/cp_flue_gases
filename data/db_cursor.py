46e5850b825a44b8e703050e2e57fbdf29646320
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-08-19 08:07:21 -0300

    Context manager

--- data/db_cursor.py
+++ data/db_cursor.py
@@ -2,4 +2,20 @@ import sqlite3
 
 
 class DBCursor:
-    pass
+    """
+        This 'DatabaseConnection' context manager returns a cursor.
+    """
+    def __init__(self, host: str):
+        self.host = host
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        return self.connection.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_tb or exc_val or exc_type:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
