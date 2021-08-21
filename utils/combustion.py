50cd1bcf4a58eddaee44ed96e466babce44ba0f6
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-08-21 07:59:09 -0300

    Minor coefficients added

--- utils/combustion.py
+++ utils/combustion.py
@@ -1,6 +1,10 @@
+import logging
+log = logging.getLogger("cp_flue_gases.combustion")
+
 from utils.fuel import Fuel
 from math import log as loga
 from math import exp
+from utils.errors import *
 
 
 class SingleTCombustion:
@@ -8,7 +12,44 @@ class SingleTCombustion:
         """A class to handle single temperatures combustion.
         :param n: dilution factor
         :param T: temperature in K"""
-        pass
+        try:
+            if not isinstance(fuel, Fuel):
+                raise InvalidFuel("The provided fuel isn't a Fuel object.")
+            self.fuel = fuel
+            self.n = float(n)
+            self.T = float(T)
+        except:
+            log.critical("An exception was raised.")
+            raise
+        else:
+            log.debug("SingleTCombustion object succcessfully instanciated. Calculating mayor coefficients.")
+        
+    
+
+    # Minor coefficients methods
+    def b_m(self) -> float:
+        """Returns the mass ratio of N2 to total flue gas."""
+        m_N = 0.767 * self.fuel.min_air + self.fuel.kN
+        m_tot_flue_gas = self.fuel.total_flue_gas(self.n)
+        return m_N / m_tot_flue_gas
+    
+    def c_m(self) -> float:
+        """Returns the mass ratio of H2O to total flue gas."""
+        m_H = 8.938 * self.fuel.kH + self.fuel.kM
+        m_tot_flue_gas = self.fuel.total_flue_gas(self.n)
+        return m_H / m_tot_flue_gas
+    
+    def d_m(self) -> float:
+        """Returns the mass ratio of SO2 to total flue gas."""
+        m_S = 2 * self.fuel.kS
+        m_tot_flue_gas = self.fuel.total_flue_gas(self.n)
+        return m_S / m_tot_flue_gas
+    
+    def f_m(self) -> float:
+        """Returns another coefficient correct the excess air amount."""
+        m_air_steo = self.fuel.min_air
+        m_flue_gas = self.fuel.total_flue_gas(self.n)
+        return (m_air_steo * (self.n - 1)) / m_flue_gas
     
 
 class Approximations:
@@ -42,4 +83,3 @@ class Approximations:
         """A constant to adjust the coefficient fA.
         :param T: temperature in K"""
         return 0.7124 * 1.00011**T * T**0.051
-
