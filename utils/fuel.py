4e999d4c7d914e0b9ac27328bf0220902a9ed054
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-08-21 06:53:29 -0300

    Fuel can be correctly initialized now.

--- utils/fuel.py
+++ utils/fuel.py
@@ -1,17 +1,47 @@
+import logging
+log = logging.getLogger("cp_flue_gases.fuel")
+
 from typing import Dict
+from data.db_manager import Database
+from utils.errors import *
 
 
 class Fuel:
-    def __init__(
-        self,
-        name: str,
-        kC: float,
-        kH: float,
-        kO: float,
-        kN: float,
-        kS: float,
-        kM: float,
-        kAsh: float
-    ) -> None:
-        self.name = name.lower()
-        self.kC, self.kH, self.kO, self.kN, self.kS, self.kM, self.kAsh = kC, kH, kO, kN, kS, kM, kAsh
+    def __init__(self, name: str, db: Database)-> None:
+        """Initializes the object.
+        :param name: the fuel's name.
+        :param db: a Database object from where to extract the information."""
+        log.debug(f"Initializing a Fuel object. Searching for an entry like {name.title()} in the database.")
+        try:
+            self.name = name.lower()
+            from_db = db.get_fuel_composition(name.lower())
+            self.kC, self.kH, self.kO, self.kN, self.kS, self.kM, self.kAsh = from_db
+            if not self.has_integrity():
+                raise FuelIntegrityError("The given fuel's composition doesn't add up to a 100%.")
+        except Exception:
+            log.critical("An exception was raised")
+            raise
+        else:
+            log.debug("Fuel object correctly initialized. Calculating their minimum air and gas.")
+            # Calculating the minimum amount of air per kg of fuel.
+            self.min_air = (2.9978 * self.kH - 0.3747 * (self.kS - self.kO) + self.kC) * 11.445
+            # Calculating the minimum total gas after burning this fuel.
+            self.min_gas_total = self.min_air + (1 - self.kAsh)
+
+    def has_integrity(self) -> bool:
+        """Checks if the given composition adds up to a 100% (1)."""
+        total_comp = self.kC + self.kH + self.kO + self.kN + self.kS + self.kM + self.kAsh
+        if total_comp != 1:
+            log.critical(f"This {self.name.title()} fuel has a composition that doesn't add up to 100%.")
+            return False
+        log.debug(f"This {self.name.title()} fuel has integrity")
+        return True
+    
+    def __repr__(self) -> str:
+        return f"""<<FUEL OBJECT NAMED {self.name.upper()}>>"""
+
+        
+        
+        
+
+
